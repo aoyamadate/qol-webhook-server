91c9e32743413f85bea0f59eac10160e3912be5c
aoyamadate <aoshin.dailysystem@gmail.com>
2025-05-19 18:46:11 +0900

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# あなたのGAS Webhookエンドポイント
 GAS_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbz3ATERSJlJBzg8iYKJ2n3gCYIx5orU6F3Boh7yybK47loa2c2adyxT8xIKPaYlOpd0/exec"
 
 @app.route("/", methods=["POST"])
@@ -11,3 +10,7 @@ def relay():
     data = request.json
     res = requests.post(GAS_WEBHOOK_URL, json=data)
     return {"status": "relayed", "gas_response": res.text}, 200
+
+# これを追加すると確実に動くことがあります（Vercelの仕様）
+if __name__ == "__main__":
+    app.run()
